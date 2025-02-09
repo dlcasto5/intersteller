50aebf0e03319fe24da06a47469b95841d310344
Devon Casto <dlcasto4@gmail.com>
2025-02-09 17:10:34 -0500

    Update m.js

--- assets/js/m.js
+++ assets/js/m.js
@@ -33,7 +33,7 @@ if (nav) {
 <div class="f-nav-right">
     <a class="navbar-link" href="games.html"><i class="fa-solid fa-gamepad navbar-icon"></i><an>&#71;&#97;</an><an>&#109;&#101;&#115;</an></a>
     <a class="navbar-link" href="apps.html"><i class="fa-solid fa-phone navbar-icon"></i><an>&#65;&#112;</an><an>&#112;&#115;</an></a>
-    ${window.top.location.pathname === "/tabs.html" ? "" : '<a class="navbar-link" href="/./rx"><i class="fa-solid fa-laptop navbar-icon"></i><an>&#84;&#97;</an><an>&#98;&#115;</an></a>'}
+    ${window.top.location.pathname === "/tabs.html" ? "" : '<a class="navbar-link" href="/tabs.html"><i class="fa-solid fa-laptop navbar-icon"></i><an>&#84;&#97;</an><an>&#98;&#115;</an></a>'}
     <a class="navbar-link" href="settings.html"><i class="fa-solid fa-gear navbar-icon settings-icon"></i><an>&#83;&#101;&#116;</an><an>&#116;&#105;&#110;&#103;</an></a>
 </div>`;
   nav.innerHTML = html;
